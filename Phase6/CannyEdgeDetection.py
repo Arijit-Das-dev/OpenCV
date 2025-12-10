e98fe0935418d8c85fd2dd0c3ae8e8d9b11f1b45
Arijit-Das-Dev <arijitdas.das99@gmail.com>
2025-12-10 23:46:53 +0530

    Updated

--- Phase6/CannyEdgeDetection.py
+++ Phase6/CannyEdgeDetection.py
@@ -7,10 +7,24 @@ That function used for detecting all the edges of any image.
 
 Usage :
 
-1> 
+1> Edge detection
+2> Seperate objects in a image
+3> Feature Extraction
 
 '''
 
 
 import cv2
 
+image = cv2.imread("C:\\Users\\abijit\\Downloads\\FlowerImage.jpg", cv2.IMREAD_GRAYSCALE)
+
+if image is not None:
+
+    edge = cv2.Canny(image, 50, 150)
+    cv2.imshow("image",edge)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+else:
+
+    print("Error")
